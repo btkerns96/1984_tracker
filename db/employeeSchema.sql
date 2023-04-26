36f1a9368dbcf48d1a31e14b95f854a2e0d1b740
BrandonKerns <btkerns96@gmail.com>
2023-04-26 13:19:18 -0700

    Updated code

---
+++ db/employeeSchema.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS employee_DB;
+CREATE database employee_DB;
+
+USE employee_DB;
+
---
+++ employeeManager.js
@@ -0,0 +1,15 @@
+// List the dependencies here.
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const consoleTable = require('console.table');
+const util = require('util');
+
+// Create the connection to MySQL WorkBench
+let connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'employee_DB'
+});
+
